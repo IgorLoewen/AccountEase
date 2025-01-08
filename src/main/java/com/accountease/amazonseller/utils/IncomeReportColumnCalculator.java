9d7107a97c8b9cff3adae8f1eaf8b81761aaa01d
Gior UG <GiorUg@ALs-iMac.local>
2025-01-08 18:19:00 +0100

    Created more sums filter. Still in process

--- src/main/java/com/accountease/amazonseller/utils/IncomeReportColumnCalculator.java
+++ src/main/java/com/accountease/amazonseller/utils/IncomeReportColumnCalculator.java
@@ -26,6 +26,12 @@ public class IncomeReportColumnCalculator {
             processAndPrintReport(ReportSettingsFactory.createAdjustmentsFees(), data);
             processAndPrintReport(ReportSettingsFactory.getTotalServiceFees(), data);
             processAndPrintReport(ReportSettingsFactory.getTotalStorageAndServiceFeesForAmazonFulfillment(), data);
+            processAndPrintReport(ReportSettingsFactory.getTotalRefundsForShippingCredits(), data);
+            processAndPrintReport(ReportSettingsFactory.getTotalRefundsForPromotionalDiscounts(), data);
+            processAndPrintReport(ReportSettingsFactory.getTotalShippingCreditNotes(), data);
+            processAndPrintReport(ReportSettingsFactory.getTotalFBALogisticsInventoryCredits(), data);
+            processAndPrintReport(ReportSettingsFactory.getTotalRefundsForAmazonShippedItems(), data);
+            processAndPrintReport(ReportSettingsFactory.getTotalRefundsForAmazonTransactionFees(), data);
 
         } catch (IOException e) {
             System.err.println(e.getMessage());
